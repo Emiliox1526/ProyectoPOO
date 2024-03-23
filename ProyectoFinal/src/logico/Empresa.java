0f89f4f430b519455faabc5438f8267ea41c8ac6
ezequ <ezequ@Eze>
2024-03-23 15:22:59 -0400

    Creando y inicializacion de las clases: Cliente, Contrato, Proyecto, Emp

--- ProyectoFinal/src/logico/Empresa.java
+++ ProyectoFinal/src/logico/Empresa.java
@@ -1,5 +1,90 @@
 package logico;
 
+import java.util.ArrayList;
+
 public class Empresa {
 	
+	private ArrayList<Cliente>misClientes;
+	private ArrayList<Contrato>misContratos;
+	private ArrayList<Trabajador>misTrabajadores;
+	private ArrayList<Proyecto>misProyectos;
+	private static Empresa empresa = null;
+	public static int idContrato;
+	public static int idProyecto;
+	
+	public Empresa() {
+		super();
+		misClientes = new ArrayList<>();
+		misContratos = new ArrayList<>();
+		misTrabajadores = new ArrayList<>();
+		misProyectos = new ArrayList<>();
+		idContrato = 1;
+		idProyecto = 1;
+	}
+	
+	public static Empresa getInstance() {
+		if(empresa == null) {
+			empresa = new Empresa();
+		}
+		return empresa;
+	}
+
+	public ArrayList<Cliente> getMisClientes() {
+		return misClientes;
+	}
+
+	public void setMisClientes(ArrayList<Cliente> misClientes) {
+		this.misClientes = misClientes;
+	}
+
+	public ArrayList<Contrato> getMisContratos() {
+		return misContratos;
+	}
+
+	public void setMisContratos(ArrayList<Contrato> misContratos) {
+		this.misContratos = misContratos;
+	}
+
+	public ArrayList<Trabajador> getMisTrabajadores() {
+		return misTrabajadores;
+	}
+
+	public void setMisTrabajadores(ArrayList<Trabajador> misTrabajadores) {
+		this.misTrabajadores = misTrabajadores;
+	}
+
+	public ArrayList<Proyecto> getMisProyectos() {
+		return misProyectos;
+	}
+
+	public void setMisProyectos(ArrayList<Proyecto> misProyectos) {
+		this.misProyectos = misProyectos;
+	}
+
+	public static Empresa getEmpresa() {
+		return empresa;
+	}
+
+	public static void setEmpresa(Empresa empresa) {
+		Empresa.empresa = empresa;
+	}
+
+	public static int getIdContrato() {
+		return idContrato;
+	}
+
+	public static void setIdContrato(int idContrato) {
+		Empresa.idContrato = idContrato;
+	}
+
+	public static int getIdProyecto() {
+		return idProyecto;
+	}
+
+	public static void setIdProyecto(int idProyecto) {
+		Empresa.idProyecto = idProyecto;
+	}
+	
+	
+
 }
