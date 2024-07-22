6311d33208c0d1b0c9cd9c763675341a7b55e3e0
Danim <Danim@DESKTOP-MEBHPIV>
2024-07-22 16:31:29 -0400

    Class COnect prueba

--- ProyectoFinal/src/Conexion/Conect.java
+++ ProyectoFinal/src/Conexion/Conect.java
@@ -2,21 +2,42 @@ package Conexion;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class Conect {
 	
 	Connection con;
-	public Conect(){
-	try{
-		
-		Class.forName("com.mysql.cj.jdbc.Driver");
-		con = DriverManager.getConnection("jdbc:mysql://localhost:3306/Proyecto_Final", "admin", "admin");
-		}	catch(Exception e) {
-			System.err.println("Error" +e);
+	public Conect() {
+		try {
+			// Cargar el controlador correcto para SQL Server
+			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+			con = DriverManager.getConnection("jdbc:sqlserver://192.168.100.118:1433;"
+					+ "database=gestion_paquetes;"
+					+ "user=;" // TU USER
+					+ "password=;" // TU CLAVE
+					+ "encrypt=true;"
+					+ "trustServerCertificate=true;"
+					+ "loginTimeout=30;");
+
+
+			Statement statement = con.createStatement();
+			String selectSql = "SELECT * FROM lugar";
+			ResultSet resultSet = statement.executeQuery(selectSql);
+
+
+			while (resultSet.next()) {
+				int id = resultSet.getInt("id_lugar");
+				String nombre = resultSet.getString("nombre_lugar");
+				System.out.println("ID: " + id + ", Nombre: " + nombre);
+			}
+		} catch (Exception e) {
+			System.err.println("Error: " + e);
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		Conect cn = new Conect();
 	}
 }
+
